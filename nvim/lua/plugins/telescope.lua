6d0e2f1d33816b370267968f96679c92f8bba430
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-04-06 16:42:17 -0400

    changing telescope config to include dotfiles (how ironic

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,27 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local telescope = require("telescope")
 
-      telescope.setup()
+            telescope.setup {
+                pickers = {
+                    find_files = {
+                        hidden = true
+                    }
+                }
+            }
 
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Telescope: Live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Telescope: Find buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags,  { desc = "Telescope: Help tags" })
-    end,
-  }
+            local builtin = require("telescope.builtin")
+
+
+
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Find buffers" })
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
+        end,
+    }
 }
-
